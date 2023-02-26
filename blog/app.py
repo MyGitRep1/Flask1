8104b252513d67699f0c252d7efe33ef2b08534e
MyGitRep1 <uzinovev26@gmail.com>
2023-02-26 19:52:16 +0300

    Added: @app.route(/user/)

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route("/greet/<name>/")
-def greet_name(name: str):
-    return f"Hello {name}!"
+@app.route("/user/")
+def read_user():
+    name = request.args.get("name")
+    surname = request.args.get("surname")
+    return f"User {name or '[no name]'} {surname or '[no surname]'}"
