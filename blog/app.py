3d9278f2ee7c57f27eb40294ce80c7ce23d64d6b
MyGitRep1 <uzinovev26@gmail.com>
2023-02-26 19:36:24 +0300

    Created: 'greet GitHub'

--- blog/app.py
+++ blog/app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'Hello!'
+@app.route("/greet/<name>/")
+def greet_name(name: str):
+    return f"Hello {name}!"
