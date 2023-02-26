56493a926ef2ea1a2e171b5a01d94fc3dfe75129
MyGitRep1 <uzinovev26@gmail.com>
2023-02-26 20:33:40 +0300

    Changed view.py

--- blog/view.py
+++ blog/view.py
@@ -4,27 +4,13 @@ import app as app
 from werkzeug.exceptions import BadRequest
 
 
-@app.route("/power/")
-def power_value():
-    x = request.args.get("x") or ""
-    y = request.args.get("y") or ""
-    if not (x.isdigit() and y.isdigit()):
-        app.logger.info("invalid values for power: x=%r and y=%r", x, y)
-        raise BadRequest("please pass integers in `x` and `y` query params")
-    x = int(x)
-    y = int(y)
-    result = x ** y
-    app.logger.debug("%s ** %s = %s", x, y, result)
-    return str(result)
+@app.route("/divide-by-zero/")
+def do_zero_division():
+    return 1 / 0
 
 
-# GET (no values passed) http://127.0.0.1:5000/power/
-#  < HTTP/1.0 400 BAD REQUEST
-#  - Bad Request
-#    please pass integers in `x` and `y` query params
-#  [log]: INFO in app: invalid values for power: x='' and y=''
-
-# GET (pass valid data) http://127.0.0.1:5000/power/?x=7&y=3
-# < HTTP/1.0 200 OK
-# - 343
-# [log]: DEBUG in app: 7 ** 3 = 343
+@app.errorhandler(ZeroDivisionError)
+def handle_zero_division_error(error):
+    print(error) # prints str version of error: 'division by zero'
+    app.logger.exception("Here's traceback for zero division error")
+    return "Never divide by zero!", 400
